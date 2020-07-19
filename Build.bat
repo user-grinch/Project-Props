babffa7b5224fed558224ef6dd6df1c8fa01e179
Grinch_ <user.grinch@gmail.com>
2020-07-19 07:46:01 +0600

    Fixed the build.bat

--- Build.bat
+++ Build.bat
@@ -1,11 +1,12 @@
 @echo off
-set workingDir=%CD%\Project Props\data\
+set workingDir=%CD%\Project Props\
 
 REM Convert IPL files
 echo Converting IPL files
 
-FOR %%i IN ("%workingDir%\*.ipl") DO (
-	start %CD%\tools\fastman92_processor.exe /file_type ipl /input_type text /input_game GAME_EXACT_GTASA_PC /input_filename "%workingDir%\%%~ni.ipl" /output_type binary /output_game GAME_EXACT_GTASA_PC /output_filename "%workingDir%\img_src\%%~ni.ipl"
+FOR %%i IN ("%workingDir%\raw\*.ipl") DO (
+	start %CD%\tools\fastman92_processor.exe /file_type ipl /input_type text /input_game GAME_EXACT_GTASA_PC /input_filename "%workingDir%\raw\%%~ni.ipl" /output_type binary /output_game GAME_EXACT_GTASA_PC /output_filename "%workingDir%\data\%%~ni.ipl"
 )
 
+echo Converting finished
 pause
