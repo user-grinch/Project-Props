f83ad55261e050343f8927acb0f64e62ecddce98
Grinch_ <user.grinch@gmail.com>
2020-10-13 23:39:23 +0600

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -3,10 +3,10 @@ set workingDir=%CD%\modloader\Project Props\
 
 REM Convert IPL files
 echo Converting IPL files
-
+echo ------------------------------------
 FOR %%i IN ("%workingDir%\raw\*.ipl") DO (
-	start %CD%\fastman92_processor.exe /file_type ipl /input_type text /input_game GAME_EXACT_GTASA_PC /input_filename "%workingDir%\raw\%%~ni.ipl" /output_type binary /output_game GAME_EXACT_GTASA_PC /output_filename "%workingDir%\data\%%~ni.ipl"
+	cmd /C ""%CD%\fastman92_processor.exe" /file_type ipl /input_type text /input_game GAME_EXACT_GTASA_PC /input_filename "%workingDir%\raw\%%~ni.ipl" /output_type binary /output_game GAME_EXACT_GTASA_PC /output_filename "%workingDir%\data\%%~ni.ipl""    
 )
-
+echo ------------------------------------
 echo Converting finished
 pause
