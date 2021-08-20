3802841022579f368fc44768c047fa6a8c387dbf
Grinch_ <user.grinch@gmail.com>
2021-08-20 08:40:49 +0600

    tidy up code

--- src/ProjectProps.cpp
+++ src/ProjectProps.cpp
@@ -11,39 +11,35 @@ std::ofstream lg;
 
 ProjectProps::ProjectProps()
 {
-	Events::shutdownRwEvent += []
-	{
-		LOG("Log ended");
-	};
-
-	Events::initGameEvent += []
-	{
-		lg.open("ProjectProps.log");
-
-		LOG("Log started");
-
-		SI_Error rc = ini.LoadFile("ProjectProps.ini");
-		if (rc < 0)
-		{
-			LOG("ProjectProps.ini not found.")
-			return;
-		}
-
-		ini.GetAllSections(sections);
-
-		// Object.dat loader
-		if (std::filesystem::exists("ProjectProps.dat"))
-		{
-			Call<0x005B5360>("ProjectProps.dat");
-		}
-		else
-		{
-			LOG("ProjectProps.dat not found.");
-		}
-	};
+	static bool bInit = true;
 
 	Events::processScriptsEvent += []()
 	{
+		if (bInit)
+		{
+			lg.open("ProjectProps.log");
+
+			SI_Error rc = ini.LoadFile("ProjectProps.ini");
+			if (rc < 0)
+			{
+				LOG("ProjectProps.ini not found.")
+				return;
+			}
+
+			ini.GetAllSections(sections);
+
+			// Object.dat loader
+			if (std::filesystem::exists("ProjectProps.dat"))
+			{
+				Call<0x5B5360>("ProjectProps.dat");
+			}
+			else
+			{
+				LOG("ProjectProps.dat not found.");
+			}
+			bInit = false;
+		}
+
 		for (CObject* pObj : CPools::ms_pObjectPool)
 		{
 			ProcessDynamicObject(pObj);
@@ -70,7 +66,9 @@ void ProjectProps::ProcessDynamicObject(CObject* pObj)
 			if (xdata.m_bProcessed)
 			{
 				if (pObj == NULL || xdata.m_pEntity == NULL)
+				{
 					continue;
+				}
 
 				if (bVisibleFlag == VISIBLE_BROKEN || bVisibleFlag == VISIBLE_LIFTED)
 				{
@@ -80,7 +78,9 @@ void ProjectProps::ProcessDynamicObject(CObject* pObj)
 							|| (pObj->m_nPhysicalFlags.bOnSolidSurface == 0 && bVisibleFlag == VISIBLE_LIFTED))
 						{
 							if (xdata.m_pEntity != NULL)
+							{
 								xdata.m_pEntity->Remove();
+							}
 							xdata.m_bShown = false;
 						}
 					}
@@ -90,7 +90,9 @@ void ProjectProps::ProcessDynamicObject(CObject* pObj)
 							|| (pObj->m_nPhysicalFlags.bOnSolidSurface == 1 && bVisibleFlag == VISIBLE_LIFTED))
 						{
 							if (xdata.m_pEntity != NULL)
+							{
 								xdata.m_pEntity->Add();
+							}
 							xdata.m_bShown = true;
 						}
 					}
@@ -98,7 +100,7 @@ void ProjectProps::ProcessDynamicObject(CObject* pObj)
 			}
 			else
 			{
-				int request_model = std::stoi(ini.GetValue(it->pItem, "model", "-1"));
+				int requestModel = std::stoi(ini.GetValue(it->pItem, "model", "-1"));
 
 				float offsetX = std::stof(ini.GetValue(it->pItem, "X", "0.0"));
 				float offsetY = std::stof(ini.GetValue(it->pItem, "Y", "0.0"));
@@ -108,16 +110,16 @@ void ProjectProps::ProcessDynamicObject(CObject* pObj)
 
 				int magic_num = rand() % (chance + 1 - 1) + 1;
 
-				if (magic_num != 1 || request_model == -1) // random value
+				if (magic_num != 1 || requestModel == -1) // random value
 				{
 					xdata.m_bIgnore = true;
 					continue;
 				}
 
-				CStreaming::RequestModel(request_model, eStreamingFlags::GAME_REQUIRED);
+				CStreaming::RequestModel(requestModel, eStreamingFlags::GAME_REQUIRED);
 				CStreaming::LoadAllRequestedModels(false);
 
-				xdata.m_pEntity = CObject::Create(request_model);
+				xdata.m_pEntity = CObject::Create(requestModel);
 				CObject::PlacePhysicalRelativeToOtherPhysical(pObj, (CObject*)xdata.m_pEntity, CVector(offsetX, offsetY, offsetZ));
 				xdata.m_pEntity->SetHeading(rotation);
 
@@ -125,9 +127,12 @@ void ProjectProps::ProcessDynamicObject(CObject* pObj)
 				{
 					xdata.m_pEntity->Remove();
 				}
-				else xdata.m_bShown = true;
+				else 
+				{
+					xdata.m_bShown = true;
+				}
 
-				CStreaming::SetModelIsDeletable(request_model);
+				CStreaming::SetModelIsDeletable(requestModel);
 				xdata.m_bProcessed = true;
 			}
 		}
