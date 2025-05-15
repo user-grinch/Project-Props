68c285da91a41df7d41c4288868075e9904555d9
Zeneric <zeneric1123@gmail.com>
2025-05-15 11:06:16 -0400

    asd

--- Install ProjectProps.bat
+++ Install ProjectProps.bat
@@ -7,35 +7,27 @@ echo Installation Methods:
 echo 1. Weeds / 2dGroundObjects / ProcObj
 echo 2. Weeds / 2dGroundobjects
 echo 3. ProcObj only
-echo 4. None of the Above (Mininal Installation)
- 
 echo.
 echo Weeds = Plants.
 echo 2dGroundObjects = manholes, litters, oilspills, etc.
 echo ProcObj = Objects placed randomly on roads and pavements.
 echo.
 echo.
-choice  /C 1234 /M "Select an installation method."
+choice  /C 123 /M "Select an installation method."
 
 if "%ERRORLEVEL%"=="1" (
-	MOVE /Y "%CD%\optional\lae_stream7.ipl" "%workingDir%ipl_src\"
-	MOVE /Y "%CD%\optional\lae_stream9.ipl" "%workingDir%ipl_src\"
-	MOVE /Y "%CD%\optional\procobj.dat" "%workingDir%"
-	MOVE /Y "%CD%\optional\surfinfo.dat" "%workingDir%"
-	@DEL /S /Q "%workingDir%\ipl_src\lae_stream8.ipl"
+	MOVE /Y "%CD%\optional\Lae_stream7.ipl" "%workingDir%ipl_src\"
+	MOVE /Y "%CD%\optional\lae_stream8.ipl" "%workingDir%ipl_src\"
+	MOVE /Y "%CD%\optional\procobj.dat" "%workingDir%files\"
+	MOVE /Y "%CD%\optional\surfinfo.dat" "%workingDir%files\"
 )
 if "%ERRORLEVEL%"=="2" (
 	MOVE /Y "%CD%\optional\lae_stream7.ipl" "%workingDir%ipl_src\"
-	MOVE /Y "%CD%\optional\lae_stream9.ipl" "%workingDir%ipl_src\"
-	@DEL /S /Q "%workingDir%\ipl_src\lae_stream8.ipl"
+	MOVE /Y "%CD%\optional\lae_stream8.ipl" "%workingDir%ipl_src\"
 )
 if "%ERRORLEVEL%"=="3" (
-	MOVE /Y "%CD%\optional\procobj.dat" "%workingDir%"
-	MOVE /Y "%CD%\optional\surfinfo.dat" "%workingDir%"
-	@DEL /S /Q "%workingDir%\ipl_src\lae_stream8.ipl"
-)
-if "%ERRORLEVEL%"=="4" (
-	@DEL /S /Q "%workingDir%\ipl_src\lae_stream8.ipl"
+	MOVE /Y "%CD%\optional\procobj.dat" "%workingDir%files\"
+	MOVE /Y "%CD%\optional\surfinfo.dat" "%workingDir%files\"
 )
 
 echo ---------------------------------
